3a268c57443de8b505b668a563b44af6045b4c9b
macbook <dinhbinhnguyen.main@gmail.com>
2025-06-06 00:25:05 +0700

    from macmini

--- src/robot/browser_actions.py
+++ src/robot/browser_actions.py
@@ -6,7 +6,7 @@ from src.my_types import RobotTaskType, BrowserWorkerSignals
 def do_launch_browser(page: Page, task: RobotTaskType, signals: BrowserWorkerSignals):
     try:
         signals.progress_signal.emit(task, "Launching ...", 0, 1)
-        page.goto(task.action_payload.get("url", ""), wait_until="networkidle")
+        page.goto(task.action_payload.get("url", ""), wait_until="domcontentloaded")
         page.wait_for_event("close", timeout=0)
         signals.progress_signal.emit(task, "Closed!", 1, 1)
     except Exception as e:
